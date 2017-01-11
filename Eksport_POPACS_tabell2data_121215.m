ff77d81c26b10000b00d04b9542d76032476f7d3
larslefstad <larslefstad@gmail.com>
2017-01-11 11:21:58 +0100

    Add files via upload

---
+++ Eksport_POPACS_tabell2data_121215.m
@@ -0,0 +1,14 @@
+cd('M:\POPACS\Matlab\Output\POPACS_tekstfiler');
+
+fprintf(fid11,'%3d\t %3d\t %3d\t %3d\t %3d\t %3d\t %3d\t %3d\t %3d\n',PasID,t2_bloodloss_perop,t2_bloodloss_postop ,t2_veske_perop,t2_veske_PO,t2_ery_perop,t2_ery_PO,t2_plasma_perop,t2_plasma_PO);
+
+fprintf(fid12,'%3d\t %3d\n',PasID,t2_epidural_peropNY);
+
+fprintf(fid13,'%3d\t %3d\n',PasID,t2_epidural_PONY);
+ 
+fprintf(fid14,'%3d\t %3d\n',PasID,t2_opioid_postop);
+ 
+fprintf(fid15,'%3d\t %3d\n',PasID,t2_fentanyl_totalNY);
+
+
+
